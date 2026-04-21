708a229fcb45a801165be04c9c80b9f802eae044
VeuxIPTV <saffartem2008@gmail.com>
2026-04-21 18:15:48 +0300

    r

--- HLS/Tunnel_Auto.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /min ssh -i ~/.ssh/lhr_key -R 80:localhost:8080 nokey@localhost.run
--- HLS/tunnel.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:loop
-echo Запуск SSH-туннеля...
-ssh -i ~/.ssh/lhr_key -R 80:localhost:8080 nokey@localhost.run
-echo Туннель упал. Переподключение через 5 секунд...
-timeout /t 5 >nul
-goto loop
