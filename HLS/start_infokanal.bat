b19ba98505566d71033b8c690779f3ed3e433cca
VeuxIPTV <saffartem2008@gmail.com>
2026-04-21 20:59:34 +0300

    x

--- HLS/start_infokanal.bat
+++ HLS/start_infokanal.bat
@@ -2,21 +2,20 @@
 title Инфоканал (автозапуск туннеля)
 cd /d "C:\Репозиторий github\TV\HLS"
 
-:: Проверяем, запущен ли сервер (live_stream.py). Если нет — запускаем в фоне.
+:: Проверяем, запущен ли сервер (live.py). Если нет — запускаем в фоне.
 tasklist /fi "imagename eq python.exe" | find "live_stream.py" >nul
 if errorlevel 1 (
     echo [СЕРВЕР] Запуск live_stream.py...
-    start /min py live_stream.py
+    start /min py live.py
     timeout /t 5
 ) else (
     echo [СЕРВЕР] Уже работает.
 )
 
-:: Бесконечный цикл для туннеля
 :loop
-echo [ТУННЕЛЬ] Подключение к serveo.net с доменом veuxiptv-tv...
-ssh -i "%USERPROFILE%\.ssh\serveo_key" -o ServerAliveInterval=30 -o TCPKeepAlive=yes -o ExitOnForwardFailure=yes -R veuxiptv-tv:80:localhost:8080 serveo.net
+echo [Tunnel] Connect to Tunnel...
+ssh -i "$env:USERPROFILE\.ssh\serveo_key" -o ServerAliveInterval=30 -R veuxtv:80:localhost:8080 serveo.net
 
-echo [ТУННЕЛЬ] Соединение разорвано. Перезапуск через 10 секунд...
+echo [Tunnel] Connection close. Reboot to 10 second...
 timeout /t 10
 goto loop
