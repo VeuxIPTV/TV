fd2d259da6304f6d6c253f13ded993bcb9693b6d
VeuxIPTV <saffartem2008@gmail.com>
2026-04-22 13:33:47 +0300

    a

--- HLS/Tunnel_Auto.bat
+++ HLS/Tunnel_Auto.bat
@@ -3,7 +3,7 @@ title Tunnel Serveo (авто-перезапуск)
 
 :loop
 echo Connect to Tunnel...
-ssh -i "%USERPROFILE%\.ssh\serveo_key" -o ServerAliveInterval=30 -o TCPKeepAlive=yes -R veuxtv:80:localhost:8080 serveo.net
+ssh -i "%USERPROFILE%\.ssh\serveo_key" -o ServerAliveInterval=30 -o TCPKeepAlive=yes -R veuxtv:8080:localhost:8080 serveo.net
 echo Connection Close. Reboot...
 timeout /t 0
 goto loop
