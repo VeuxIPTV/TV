8a1124cc9ae923ffb305310387dcdc28de2ace06
VeuxIPTV <saffartem2008@gmail.com>
2026-04-24 15:15:24 +0300

    update

--- HLS/Live_channel.bat
+++ HLS/Live_channel.bat
@@ -11,9 +11,17 @@ if errorlevel 1 (
     echo [Server] The translator is running.
 )
 
+taskkill /f /im ssh.exe 2>nul
+timeout /t 1
+
 :loop
-echo [Tunnel] Connect to Tunnel...
-ssh -i "%USERPROFILE%\.ssh\serveo_key" -o ServerAliveInterval=30 -o TCPKeepAlive=yes -R veuxtv:80:localhost:8080 serveo.net
-echo [Tunnel] Connection Close. Reboot to 5 second...
-timeout /t 5
+echo Connect to Tunnel...
+ssh -i "%USERPROFILE%\.ssh\serveo_key" ^
+    -o ServerAliveInterval=30 ^
+    -o ServerAliveCountMax=3 ^
+    -o TCPKeepAlive=yes ^
+    -o ExitOnForwardFailure=yes ^
+    -R veuxtv:80:localhost:8080 serveo.net
+echo Connection Close. Reboot...
+timeout /t 0
 goto loop
